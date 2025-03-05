527d42989f45ac21f73479857bd15e1ac4db996e
antoniorusso2 <russoantonio303@outlook.it>
2025-03-05 19:41:53 +0100

    add other routes and delete the template file and link css to home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Homepage</title>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <nav>
+                <ul>
+                    <li><a href="{{route('about')}}">About</a></li>
+                    <li><a href="{{route('contact-page')}}">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contacts'); //reale nome della rotta che deve essere uguale al file .blade.php nella cartella views
+})->name('contact-page');//name serve per quando invochiamo la funzione route ma è diverso dal nome della rotta
